c62c08d9df7cc9991d8ed5ca402f843587ea6b79
Sai Hemanth Uppala <ushemanth1@gmail.com>
2024-02-02 18:09:51 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,9 +1,9 @@
 // Set the time delay in milliseconds (e.g., 5000 milliseconds = 5 seconds)
-const redirectDelay = 5000;
+const redirectDelay = 2000;
 
 // Function to redirect to your Webflow website
 function redirectToWebflow() {
-  window.location.href = 'https://ushemanthportfolio.framer.website';
+  window.location.href = 'https://saihemanthuppala.framer.website';
 }
 
 // Set a timeout to redirect after the specified delay
